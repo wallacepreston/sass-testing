fe3008e7b019170d5a51a13fa6922ba4b2a5f738
Preston Wallace <wallace.preston@gmail.com>
2022-11-29 07:30:51 -0800

    tests passing, sass working

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='container'>
+      <div>
+        <div className='grey'></div>
+      </div>
+      <div>
+        <div className='green'></div>
+      </div>
+      <div>
+        <div className='blue'></div>
+      </div>
+    </div>
+  );
+}
+
+export { App };
